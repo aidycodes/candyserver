5d10a8db4b73c767bd2c0ef90551ba52bd6046ff
Aidy <aidycodes@gmail.com>
2022-09-15 20:38:52 +0100

    candystore server split from frontend

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.use(require('./routes'));
 const PORT = process.env.PORT || 8000
 
 // Server listens 
-app.listen(PORT, () => {
+app.listen('0.0.0.0:1', () => {
     console.log(`Server Listerning on port ${PORT}`)
 });
 
