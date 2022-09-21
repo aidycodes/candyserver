2b8d3450076d0f6d3264f1ef50f89d5189f27d06
Aidy <aidycodes@gmail.com>
2022-09-21 14:18:40 +0100

    candystore server split from frontend

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use(session({
     cookie: {
         secure: true,
         httpOnly: false,
-        domain: '.vercel.app',
+        domain: 'vercel.app',
         sameSite: process.env.ENVIRONMENT === "production" ? "none" : "lax"
     }
 }))
