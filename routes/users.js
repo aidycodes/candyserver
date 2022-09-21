3483b7a9f33b525d4038f846e5f1106fe09ee0db
Aidy <aidycodes@gmail.com>
2022-09-21 16:18:46 +0100

    candystore server split from frontend

--- routes/users.js
+++ routes/users.js
@@ -18,13 +18,13 @@ Order.belongsTo(Product, { as: "Product", foreignKey: "product_id" })
 
 // get session info
 
-// router.use(function (req, res, next) {
-//     res.header('Access-Control-Allow-Credentials', true);
-//     res.header('Access-Control-Allow-Origin', req.headers.origin);
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
-//     res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-//     next();
-// });
+router.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', "https://candystore.vercel.app/");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    next();
+});
 
 
 router.get('/protect', async (req, res) => {
