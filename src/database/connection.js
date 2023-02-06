e2fdaaef345b5de7dbd5599a4c4b6b703977a739
Aidy <aidycodes@gmail.com>
2023-02-06 13:45:28 +0000

    new db address

--- src/database/connection.js
+++ src/database/connection.js
@@ -8,7 +8,7 @@ const { Sequelize } = require('sequelize');
 //     }
 // });
 
-const sequelize = new Sequelize('postgresqlpostgresql://postgres:ad5Mj8MlIffnSlVOeY7l@containers-us-west-84.railway.app:7763/railway', { define: { timestamps: false } });
+const sequelize = new Sequelize('postgresql://postgres:ad5Mj8MlIffnSlVOeY7l@containers-us-west-84.railway.app:7763/railway', { define: { timestamps: false } });
 
 module.exports = sequelize;
 global.sequelize = sequelize;
